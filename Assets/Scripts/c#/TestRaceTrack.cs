c515a1ad3ec03e48bdc632ab26ca3bfef1fcd453
Nick Welp <nickwelp@gmail.com>
2014-05-13 16:58:39 -0500

    map maker object

---
+++ Assets/Scripts/c#/TestRaceTrack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRaceTrack : MonoBehaviour {
+    //public InsideWall as GameObject;
+   // public OutsideWall as GameObject;
+    public GameObject[] RoadWay;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 40, 80, 20), "Save Map")){
+        //   InsideWall = GameObject.Find('InsideWall');
+        //   OutsideWall = GameObject.Find('OutsideWall');
+
+            SaveMap();
+        }
+        if (GUI.Button(new Rect(20, 70, 80, 20), "Reload Map"))
+        {
+            Application.LoadLevel("_Map Maker");
+        }
+
+    }
+    void SaveMap()
+    {
+              RoadWay = GameObject.FindGameObjectsWithTag("Road");
+	        //add SaveMeshForWeb.js to Roadway, InsideWall and Outsidewall
+       //     InsideWall.AddComponent("SaveMeshForWeb");
+        //    OutsideWall.AddComponent("SaveMeshForWeb");
+            foreach(GameObject Road in RoadWay){
+                Road.AddComponent("SaveMeshForWeb");
+                }
+    }
+}
---
+++ Assets/Standard Assets/Mesh Saving/SaveMeshForWeb.js
@@ -0,0 +1,13 @@
+﻿// Add this to any object. This will save it's mesh
+// to a local file for the Web.
+ 
+var fileName = "SerializedMesh.data";
+var saveTangents = false;
+ 
+function Start()
+{
+    var inputMesh = GetComponent(MeshFilter).mesh;
+    var fullFileName = Application.dataPath + "/" + fileName;
+    MeshSerializer.WriteMeshToFileForWeb (inputMesh, fullFileName, saveTangents);
+    print ("Saved " + name + " mesh to " + fullFileName );
+}
